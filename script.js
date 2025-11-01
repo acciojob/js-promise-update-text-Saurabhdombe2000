0cdf547b97fa4636c5cc161f92510cf8a22fa364
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-11-01 21:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function sayHello() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!")
+		}, 1000)	
+	})
+}
+
+sayHello()
+.then((data) => {
+	document.getElementById("output").textContent = data;
+})
